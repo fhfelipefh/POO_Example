2bd48f81c858f11d5e01ac5699cbda93328b0c20
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2021-05-23 17:17:45 -0300

    Create Americancup.java

---
+++ src/com/company/felipe/Americancup.java
@@ -0,0 +1,19 @@
+package com.company.felipe;
+
+public class Americancup extends Containers{
+    //190 ml
+    private int capacityMax;
+    private double weight;
+    private int currentValueMl;
+
+    public Americancup() {
+        this.capacityMax = capacityMax;
+        this.weight = weight;
+        this.currentValueMl = currentValueMl;
+    }
+
+
+
+
+
+}
