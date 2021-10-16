574e0023aaa8da49d8d7f9b9fa5b0eaaed65a520
Felipe Honorio Moraes <felipehonorio55@gmail.com>
2021-10-16 16:40:28 -0300

    + can run

--- src/com/company/felipe/test/MainTest.java
+++ src/com/company/felipe/test/MainTest.java
@@ -1,7 +1,7 @@
 package com.company.felipe.test;
 
 import com.company.felipe.model.Bottle;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
